b039f1304ebffd18e4c4201b57fd7634556e65a0
Nurnabi97 <nurnabi97@gmail.com>
2021-02-01 16:27:10 +0600

    first commit

---
+++ threeDots.js
@@ -0,0 +1,11 @@
+let phone1 = ["nokia", "samsung", "oppo", "onePlus"];
+let phonePrice = [500, 1000, 1500, 3000, 5000];
+let phoneLocation = [ "dhaka", "rajsahi", "joypurhat"];
+
+let allArray = [...phone1, ...phonePrice, ...phoneLocation]; 
+console.table(allArray);
+
+//maximum value in array.
+const value = [650, 780, 360];
+const maximum = Math.max(...value);
+console.log(maximum);
