04e89e73c119701529415b8a1f4ce1ece21b8561
Nurnabi97 <nurnabi97@gmail.com>
2021-02-02 15:10:29 +0600

    dodule23

---
+++ closure.js
@@ -0,0 +1,23 @@
+function stopWatch() {
+    let count = 0;
+    return function () {
+        count++;
+        return count;
+    }
+}
+
+const clock1 = stopWatch();
+console.log(clock1());
+console.log(clock1());
+console.log(clock1());  //last count 3.
+
+
+const clock2 = stopWatch();
+console.log(clock2());  //1st count for clock2 is 1.
+console.log(clock2());  //2nd count for clock2 is 2.
+console.log(clock1());  //count start 4 because before count upto 3.
+console.log(clock1());  // count 5 for clock1
+console.log(clock2()); // last count for clock2 is 3.
+
+//this is one big table and distribute other table clock1 = 1,2,3,4,5.. clock2 = 1,2,3,4,5.. like as.
+
---
+++ join.js
@@ -0,0 +1,16 @@
+//join method.
+
+const nums = [5, 7, 9, 11, 13, "hello", "hi"];
+
+const result = nums.join("");
+console.log(result);
+
+const result1 = nums.join(" ");
+console.log(result1);
+
+const result2 = nums.join(" = ");
+console.log(result2);
+
+
+
+
